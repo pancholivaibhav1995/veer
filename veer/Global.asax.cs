f97f77dce22ea18d92d5b46e6e505ee551eaa4d3
pancholivaibhav1995 <pancholivaibhav@gmail.com>
2020-09-21 09:22:46 +0530

    design change

--- veer/Global.asax.cs
+++ veer/Global.asax.cs
@@ -23,12 +23,20 @@ namespace veer
             {
                 bundles.Add(new ScriptBundle("~/GlobalJs.js")
                      .Include("~/Scripts/jquery-3.5.1.min.js")
-                      .Include("~/Scripts/Home.js")
+                     .Include("~/Scripts/popper.min.js")
+                      .Include("~/Scripts/bootstrap.min.js")
                         );
+                bundles.Add(new StyleBundle("~/GlobalCss.css")
+                    .Include("~/Content/bootstrap.min.css")
+                       );
 
                 bundles.Add(new ScriptBundle("~/Homejs.js")
                     .Include("~/Scripts/Home.js")
                       );
+
+                bundles.Add(new StyleBundle("~/Home.css")
+                   .Include("~/Content/Home.css")
+                     );
             }
         }
     }
